c0d99c90e031825d96263261a1b7d9124482417e
Nikolas Silva <nikolas.rsilva@gmail.com>
2023-03-01 01:48:44 -0300

    Add sitemap integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,21 @@
 import { defineConfig } from 'astro/config';
+import { loadEnv } from 'vite';
 import astroI18next from "astro-i18next";
 
 // https://astro.build/config
+import sitemap from "@astrojs/sitemap";
+
+const env = loadEnv(import.meta.env.MODE, process.cwd(), '')
+
+// https://astro.build/config
 export default defineConfig({
-  integrations: [astroI18next()]
+  site: env.OLD_SITE_URL,
+  integrations: [
+    astroI18next(),
+    sitemap({
+      changefreq: "daily",
+      lastmod: new Date(),
+      priority: 0.7
+    })
+  ]
 });
