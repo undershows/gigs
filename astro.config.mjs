b1c7bde2f97ad0aa77a194189c8a229cdcc32129
Nikolas Silva <nikolas.rsilva@gmail.com>
2023-03-16 04:11:00 -0300

    Optimize images

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,22 +1,19 @@
 import { defineConfig } from 'astro/config';
 import { loadEnv } from 'vite';
-import astroI18next from "astro-i18next";
-
-// https://astro.build/config
-import sitemap from "@astrojs/sitemap";
 
 const env = loadEnv(import.meta.env.MODE, process.cwd(), 'APP_');
 
 // https://astro.build/config
+import astroI18next from "astro-i18next";
+import sitemap from "@astrojs/sitemap";
+import image from "@astrojs/image";
+
 export default defineConfig({
   base: '',
   site: env.APP_OLD_SITE_URL,
-  integrations: [
-    astroI18next(),
-    sitemap({
-      changefreq: "daily",
-      lastmod: new Date(),
-      priority: 0.7
-    })
-  ]
+  integrations: [astroI18next(), sitemap({
+    changefreq: "daily",
+    lastmod: new Date(),
+    priority: 0.7
+  }), image()]
 });
