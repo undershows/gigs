60b8e384433fde5e6bd19425956b406f022a6906
Nikolas Silva <nikolas.rsilva@gmail.com>
2023-06-01 03:08:07 -0300

    WIP: New layout setup

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,20 @@
 import { defineConfig } from 'astro/config';
 import { loadEnv } from 'vite';
-
 const env = loadEnv(import.meta.env.MODE, process.cwd(), 'APP_');
-
-// https://astro.build/config
 import astroI18next from "astro-i18next";
 import sitemap from "@astrojs/sitemap";
 import image from "@astrojs/image";
+import svelte from "@astrojs/svelte";
 
+import tailwind from "@astrojs/tailwind";
+
+// https://astro.build/config
 export default defineConfig({
   base: '',
-  site: env.APP_OLD_SITE_URL,
+  site: env.APP_SITE_URL,
   integrations: [astroI18next(), sitemap({
     changefreq: "daily",
     lastmod: new Date(),
     priority: 0.7
-  }), image()]
+  }), image(), svelte(), tailwind()]
 });
