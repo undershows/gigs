35c69e467af2930ae80bf2be30af81ff711ffd3e
brunomartins84 <focalicius@gmail.com>
2025-03-29 21:30:47 -0300

    fix: configure i18n namespace to fix missing translations in production

--- astro-i18next.config.mjs
+++ astro-i18next.config.mjs
@@ -1,5 +1,9 @@
 /** @type {import('astro-i18next').AstroI18nextConfig} */
 export default {
   defaultLocale: "pt",
-  locales: ["pt", "en"]
+  locales: ["pt", "en"],
+  i18next: {
+    ns: ["translation"],
+    defaultNS: "translation",
+  }
 };
