2f6f43ae7f6cdd91429dbaa2503e8560fe61cffc
Nikolas Silva <t.nikolas.silva@estapar.com.br>
2025-04-10 11:37:08 -0300

    fix: fix month locales and remove legacy dependencies

--- astro-i18next.config.mjs
+++
@@ -1,9 +0,0 @@
-/** @type {import('astro-i18next').AstroI18nextConfig} */
-export default {
-  defaultLocale: "pt",
-  locales: ["pt", "en"],
-  i18next: {
-    ns: ["translation"],
-    defaultNS: "translation",
-  }
-};
--- astro.config.mjs
+++ astro.config.mjs
@@ -4,16 +4,14 @@ import { loadEnv } from 'vite';
 const env = loadEnv(import.meta.env.MODE, process.cwd(), 'APP_');
 
 // https://astro.build/config
-import astroI18next from "astro-i18next";
 import sitemap from "@astrojs/sitemap";
-import image from "@astrojs/image";
 
 export default defineConfig({
   base: '',
   site: env.APP_OLD_SITE_URL,
-  integrations: [astroI18next(), sitemap({
+  integrations: [sitemap({
     changefreq: "daily",
     lastmod: new Date(),
     priority: 0.7
-  }), image()]
+  })]
 });
