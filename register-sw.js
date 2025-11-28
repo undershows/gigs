1d75184823ea7d7af6b12a2d30ff4842e71d798f
brunomartins84 <37302742+brunomartins84@users.noreply.github.com>
2025-11-28 05:10:50 +0000

    Deploying to gh-pages from @ undershows/gigs@85e2bc6cccf4da8088ae3678922490c042ba138e 🚀

--- register-sw.js
+++
@@ -1,9 +0,0 @@
-if ("serviceWorker" in navigator) {
-  window.addEventListener("load", () => {
-    navigator.serviceWorker
-      .register("/service-worker.js")
-      .then(reg => console.log("✅ Service Worker registrado com sucesso", reg))
-      .catch(err => console.error("🛑 Erro ao registrar Service Worker", err));
-  });
-}
-
