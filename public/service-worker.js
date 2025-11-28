9cb53a53256bb833cae7d24335d02377731dcb5c
brunomartins84 <focalicius@gmail.com>
2025-11-28 03:52:36 -0300

    Multiples improvements

---
+++ public/service-worker.js
@@ -0,0 +1,20 @@
+const CACHE_NAME = "undershows-static-v1";
+const urlsToCache = [
+  "/manifest.json",
+  "/images/icons/icon-192x192-v2.png",
+  "/images/icons/icon-512x512-v2.png"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener("fetch", event => {
+  if (urlsToCache.includes(new URL(event.request.url).pathname)) {
+    event.respondWith(
+      caches.match(event.request).then(response => response || fetch(event.request))
+    );
+  }
+});
